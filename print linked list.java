1cde0cbc3abfaf84951e1af98acdaef4ae56a432
Suhanipachouri <97778571+Suhanipachouri@users.noreply.github.com>
2022-10-12 22:53:54 +0530

    Create print linked list.java

---
+++ print linked list.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    // Print elements of a linked list on console
+    // head pointer input could be NULL as well
+    // for empty list
+    void display(Node head)
+    {
+        Node currnode=head;
+        while(currnode!=null){
+            System.out.print(currnode.data+" ");
+            
+            currnode=currnode.next;
+        }
+    }
+}
